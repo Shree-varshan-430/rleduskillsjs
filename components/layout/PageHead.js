94378e24014d2da4d275395ec973480e1c86c6bb
Shree-varshan-430 <shreevarshan35@gmail.com>
2025-12-31 11:46:52 +0530

    Updated main heading and meta title

--- components/layout/PageHead.js
+++ components/layout/PageHead.js
@@ -17,9 +17,9 @@
 import Head from "next/head";
 
 const PageHead = ({ headTitle }) => {
-  const defaultTitle = "RL EDU SKILLS : Education & Skills Development";
+  const defaultTitle = "RL EDU SKILLS : Master Technical & Language Prowess";
   const defaultDescription =
-    "RL EDU Skills provides Skills Development programs for Schools, Colleges, and Working Professionals to enhance their career prospects.";
+    "Master technical and language skills with RL EDU SKILLS. Enhance career readiness, communication, and professional expertise through our programs.";
   const defaultKeywords =
     "RL EDU Skills, Education, Skills Development, Career Enhancement, Professional Training, Student Programs, College Training, Skill Building, School Programs, soft skills, Technical skills, Technical Programs, Foreign language Programs, corporate training, online courses, vocational training, offline courses, categories, recent, trending , courses, recent trending courses , softskills training courses, python programming courses, internships computer science, web development, master trainer program, digital marketing courses, data science courses, machine learning courses, AI courses, cloud computing courses, cyber security courses, communication skills, leadership skills, time management skills, problem-solving skills, critical thinking skills, teamwork skills, adaptability skills, creativity skills, interpersonal skills, emotional intelligence skills, public speaking skills, negotiation skills, conflict resolution skills, Business English course, adobe tools course, programming languages course, graphic tools, Communicative English Trainer, IELTS Trainer, Digital Marketing Trainer, Web Development Trainer, become a trainer, Skills, Stories, Success, smart, learning, career-focused, studenst enchancement program, ready learners, right programming language , program , programming language, technical skills, soft skills, programming, blog, technical blog, soft skills blog, education, course, skil developement, soft skills, training, institute, rl global solution, enterprise training, corporate training,development, sucess stories";
   return (
